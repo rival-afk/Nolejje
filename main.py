7264ebef8de994dad3a6f7b16dd4b2338f4259ba
itz-rival <afa76104103@gmail.com>
2026-03-11 17:48:08 +0300

    Исправил опечатку в main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.requests import Request
-from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.exceptions import HTTPException
 from db import db_url
 import asyncpg
 
@@ -22,19 +22,27 @@ def get_subjects():
 @app.get("/info")
 def get_info():
     return {
-        "name": "School API",
-        "version": "0.1"
+        "name": "Nolejje",
+        "version": "alpha 0.1"
     }
 
 @app.get("/students")
-async def get_students():
+async def get_students(student_id: int):
     
     conn = await asyncpg.connect(db_url)
     
-    rows = await conn.fetch("SELECT id, name FROM users")
+    rows = await conn.fetch("""SELECT users.name FROM students
+                            JOIN users ON students.user_id = users.id
+                            WHERE students.id = $1""", student_id)
     
     await conn.close()
     
+    
+    if rows is None:
+        raise HTTPException(
+            status_code=404,
+            detail="No students in table :/"
+        )
     return [dict(row) for row in rows]
 
 @app.exception_handler(404)

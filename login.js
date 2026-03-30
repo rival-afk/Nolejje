678f028c0c1261711be8a9fd59020376fbb28db8
itz-rival <afa76104103@gmail.com>
2026-03-30 16:49:18 +0300

    снова небольшие корректировки + немного функционала в js

--- login.js
+++ login.js
@@ -1,6 +1,14 @@
-const loginTab = document.getElementById("login");
-const registerTab = document.getElementById("register");
+const loginTab = document.getElementById("loginTab");
+const registerTab = document.getElementById("registerTab");
+const login = document.getElementById("login");
+const register = document.getElementById("register");
 
 loginTab.addEventListener("click", function () {
-  console.log("Login")
+  login.style.display = "block";
+  register.style.display = "none";
+});
+
+registerTab.addEventListener("click", function () {
+  login.style.display = "none";
+  register.style.display = "block";
 });
